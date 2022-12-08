5bac6bf3b82ff63309d00461eaa5d859b378a3bf
E-Dawkins <ethandawkins@hotmail.co.uk>
2022-12-08 16:47:03 +1100

    More tutorial

--- Assets/ThirdPersonShooter/Scripts/Entities/Player/PlayerEntity.cs
+++ Assets/ThirdPersonShooter/Scripts/Entities/Player/PlayerEntity.cs
@@ -1,4 +1,9 @@
-﻿using UnityEngine;
+﻿using System;
+
+using ThirdPersonShooter.UI;
+
+using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace ThirdPersonShooter.Entities.Player
 {
@@ -7,11 +12,60 @@ namespace ThirdPersonShooter.Entities.Player
 		public ref Stats Stats => ref stats;
 		public Vector3 Position => transform.position;
 
-		[SerializeField] private Stats stats;
+		public Action<int> onScoreUpdated;
 
+		[SerializeField] private Stats stats;
+		[SerializeField] private InputActionReference pauseAction;
+
+		[Header("Components")] 
+		[SerializeField] private PlayerInput input;
+		[SerializeField] private Weapon weapon;
+		[SerializeField] private AudioSource hurtSource;
+		[SerializeField] private AudioSource deathSource;
+
+		public int Score => score;
+		
+		private int score;
+
+		public void AddScore(int _value)
+		{
+			score += _value;
+			onScoreUpdated?.Invoke(score);
+		}
+		
 		private void Awake()
 		{
 			stats.Start();
+			stats.onDeath += OnDeath;
+			stats.onHealthChanged += OnHealthChanged;
+			
+			weapon.SetPlayer(this);
+
+			if(GameManager.IsValid())
+				GameManager.Instance.Player = this;
+
+			if(UIManager.IsValid())
+				input.uiInputModule = UIManager.Instance.inputModule;
 		}
+
+		private void OnDestroy()
+		{
+			stats.onDeath -= OnDeath;
+			stats.onHealthChanged -= OnHealthChanged;
+		}
+
+		private void OnEnable() => pauseAction.action.performed += OnPausePerformed;
+
+		private void OnDisable() => pauseAction.action.performed -= OnPausePerformed;
+
+		private void OnPausePerformed(InputAction.CallbackContext _context)
+		{
+			if (GameManager.IsValid())
+				GameManager.Instance.TogglePaused();
+		}
+
+		private void OnDeath() => deathSource.Play();
+
+		private void OnHealthChanged(float _health) => hurtSource.Play();
 	}
 }
